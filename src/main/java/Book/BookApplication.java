86101607b7f03bd3e292f7fd05e963a35ef194ef
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2025-02-03 15:53:54 +0900

    feat: rest HTML/Controller Add

--- src/main/java/Book/BookApplication.java
+++ src/main/java/Book/BookApplication.java
@@ -2,6 +2,8 @@ package Book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class BookApplication {
@@ -9,5 +11,11 @@ public class BookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookApplication.class, args);
 	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
+
 
 }
--- src/main/java/Book/controller/RestController.java
+++ src/main/java/Book/controller/RestController.java
@@ -1,5 +1,19 @@
 package Book.controller;
 
+import Book.dto.BookDto;
+import Book.dto.BookFileDto;
+import Book.service.BookService;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URLEncoder;
@@ -8,46 +22,32 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-import Book.dto.BookFileDto;
-import Book.service.BookService;
-import jakarta.servlet.ServletOutputStream;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.ObjectUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.servlet.ModelAndView;
-
-import Book.dto.BookDto;
-
+@Slf4j
 @Controller
-public class BookController {
+public class RestController {
 
     @Autowired
     private BookService bookService;
 
-    @GetMapping("/book/openBookList.do")
+    //목록 조회
+    @GetMapping("/book")
     public ModelAndView openBookList() throws Exception {
-        ModelAndView mv = new ModelAndView("book/bookList");
+        ModelAndView mv = new ModelAndView("/book/restBookList");
+
         List<BookDto> list = bookService.selectBookList();
-        System.out.println(">>>>>>>>>>>");
-        System.out.println("Book List: " + list);
         mv.addObject("list", list);
 
         return mv;
     }
+
     // 책쓰기 화면 요청을 처리하는 메서드
-    @GetMapping("/book/openBookRegister.do")
+    @GetMapping("/book/write")
     public String openBookWrite() throws Exception {
-        return "/book/bookRegister";
+        return "/book/restBookRegister";
     }
 
     // 책 정보 저장 요청을 처리하는 메서드
-    @PostMapping("/book/insertBook.do")
+    @PostMapping("/book/write")
     public String insertBook(BookDto bookDto, MultipartHttpServletRequest request) throws Exception {
         System.out.println("책 데이터: " + bookDto);
 
@@ -61,37 +61,41 @@ public class BookController {
         // 서비스 호출
         bookService.insertBook(bookDto, request);
 
-        return "redirect:/book/openBookList.do";
+        return "redirect:/book";
     }
+
+
     // 상세 조회 요청을 처리하는 메서드
     // /board/openBoardDetail.do?boardIdx=1234
-    @GetMapping("/book/openBookDetail.do")
-    public ModelAndView openBookDetail(@RequestParam("bookId") int bookId) throws Exception {
+    @GetMapping("/book/{bookId}")
+    public ModelAndView openBookDetail(@PathVariable("bookId") int bookId) throws Exception {
         BookDto bookDto = bookService.selectBookDetail(bookId);
 
 //        System.out.println("여긴, 컨트롤러의 디테일 북이야! >>>>>> ");
 //        System.out.println(bookDto);
-        ModelAndView mv = new ModelAndView("/book/bookDetail");
+        ModelAndView mv = new ModelAndView("/book/restBookDetail");
         mv.addObject("book", bookDto);
         return mv;
     }
+
+
     // 수정 요청을 처리할 메서드
-    @PostMapping("/book/updateBook.do")
-    public String updateBook(BookDto bookDto) throws Exception {
+    @PutMapping("/book/{bookId}")
+    public String updateBook(@PathVariable("bookId") int bookId, BookDto bookDto) throws Exception {
 //        System.out.println("여긴, 컨트롤러의 업데이트 북이야! >>>>>> ");
 //        System.out.println(bookDto);
         bookService.updateBook(bookDto);
-        return "redirect:/book/openBookList.do";
+        return "redirect:/book";
     }
 
     // 삭제 요청을 처리할 메서드
-    @PostMapping("/book/deleteBook.do")
+    @DeleteMapping("/book/{bookId}")
     public String deleteBook(@RequestParam("bookId") int bookId) throws Exception {
         bookService.deleteBook(bookId);
-        return "redirect:/book/openBookList.do";
+        return "redirect:/book";
     }
 
-    @GetMapping("/book/downloadBookFile.do")
+    @GetMapping("/book/file")
     public void downloadBookFile(@RequestParam("idx") int idx, @RequestParam("bookId") int bookId, HttpServletResponse response) throws Exception {
         // 파일 정보 조회
         BookFileDto bookFileDto = bookService.selectBookFileInfo(idx, bookId);
@@ -126,5 +130,5 @@ public class BookController {
 
 
 
-}
 
+}
